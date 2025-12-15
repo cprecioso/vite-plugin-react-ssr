2132993469cfc0d7bf04e423b2f4efd809e39d6c
Carlos Precioso <github@precioso.design>
2025-12-15 12:03:56 +0100

    Remove custom out

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import { reactSsr } from "../dist/index.mjs";
 
 export default defineConfig({
-  build: { outDir: "out" },
   plugins: [reactSsr({ appModule: "./app.tsx" })],
 });
